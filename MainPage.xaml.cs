1ced044840afcd2fad625ff740d12c52e80a40cc
Arian Lackner <alackner@schueler.hakzell.at>
2023-12-14 13:10:28 +0100

    finish 14122023

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,17 +4,27 @@ namespace ArbeitsstundenXML
 {
     public partial class MainPage : ContentPage
     {
-       
+        private bool inputSaved = false;
         public MainPage(MainViewModel viewModel)
         {
             InitializeComponent();
             this.BindingContext = viewModel;
             
         }
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            if (!inputSaved)
+            {
+                string userInput = inputEntry.Text;
+                string userPassword = inputPassword.Text;
+                outputLabel.Text = "User: " + userInput;
+                inputEntry.IsReadOnly = true; //deaktiviert das mögliche Zweite eingeben
+                inputSaved = true;
+            }
+        }
 
-        
 
-        
+
     }
 
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -10,34 +10,7 @@ namespace ArbeitsstundenXML.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
-        [ObservableProperty]
-        public string _username = string.Empty;
-
-        [ObservableProperty]
-        public string _password = string.Empty;
-
-        [ObservableProperty]
-        int _stundenanzahl = 0;
-
-        [ObservableProperty]
-        public string pas = string.Empty;
-
-        [ObservableProperty]
-        public string use = string.Empty;
-
-
-
-        
-
-
-        [RelayCommand]
-        void Change()
-        {
-            this.Username = use;
-            this.Password = pas;
-        }
-
-
+       
 
     }
 }
