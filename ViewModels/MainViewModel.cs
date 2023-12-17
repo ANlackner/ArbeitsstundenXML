bc4c805213ebb4d3b3e2954a19295c08ad713873
Arian Lackner <alackner@schueler.hakzell.at>
2023-12-17 16:01:24 +0100

    created everything. saving isn't working cause xml file is not at the right place

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -12,26 +12,47 @@ namespace ArbeitsstundenXML.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
+        [ObservableProperty]
+        int _hours = 0;
 
+        [ObservableProperty]
+        string _fehler = string.Empty;
 
 
         [RelayCommand]
-       void Anmelden()
+       void Fertig()
         {
-            var rootElement = new XElement("root");
-            var nodeData = new XElement("data");
-            var valueAttribute = new XAttribute("user", "abcdefg");
-            nodeData.Add(valueAttribute);
-            nodeData.Add(new XAttribute("id", Guid.NewGuid().ToString()));
-            rootElement.Add(nodeData);
-           // rootElement.Save(hours);
+            try
+            {
+                var rootElement = new XElement("root");
+                var nodeData = new XElement("data");
+                var valueAttribute = new XAttribute("user", "abcdefg");
+                nodeData.Add(valueAttribute);
+                nodeData.Add(new XAttribute("id", Guid.NewGuid().ToString()));
+                rootElement.Add(nodeData);
+                rootElement.Save("Data.xml");
+
+
+
+                var secondnodePwd = new XElement("data");
+                secondnodePwd.Add(new XAttribute("user", Hours));
+                secondnodePwd.Add(new XAttribute("id", Guid.NewGuid().ToString()));
+                rootElement.Add(secondnodePwd);
+
+
+                rootElement.Save("data.xml");
+
+
+            }
+            catch(Exception ex)
+            {
+                this.Fehler = ex.Message;
+            }
+            
+
+            
         }
 
-        [RelayCommand]
-        void Registrieren()
-        {
-
-        }
 
     }
 }
